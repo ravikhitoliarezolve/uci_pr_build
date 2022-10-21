58c32ea9d233231e94a254f3580e45eee610ce6b
ravikhitoliarezolve <RaviKumarKhitolia@rezolve.com>
2022-10-21 14:51:31 +0530

    git hub actions

---
+++ src/main/java/com/rezolve/uci/UCI/Project/UciProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rezolve.uci.UCI.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UciProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UciProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/rezolve/uci/UCI/Project/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.rezolve.uci.UCI.Project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld   
+{  
+@RequestMapping("/")  
+public String hello()   
+{  
+return "Hello github actions";  
+}  
+}  
+
