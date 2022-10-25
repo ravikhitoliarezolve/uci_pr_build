dafcb08d22ea8cf8c18f9bc6f6e5398d97e002e5
ravikhitoliarezolve <RaviKumarKhitolia@rezolve.com>
2022-10-25 12:54:35 +0530

    initial commit for actions

---
+++ src/main/java/com/rezolve/uci/UciApplication.java
@@ -0,0 +1,13 @@
+package com.rezolve.uci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rezolve/uci/UciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezolve.uci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
